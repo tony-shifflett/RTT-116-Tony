a70a2c390fa77d30a9151e1b4317316606022a71
Tony Shifflett <charles.l.shifflett@gmail.com>
2024-10-18 14:25:40 -0500

    GLABs added

---
+++ module303/src/GLAB303_11_6/MainComparator.java
@@ -0,0 +1,17 @@
+package GLAB303_11_6;
+
+import java.util.TreeMap;
+public class MainComparator {
+
+    public static void main(String[] args) {
+
+        // Creating a treemap with a customized comparator
+        TreeMap<String, Integer> numbers = new TreeMap<>(new CustomComparator());
+
+        numbers.put("First", 1);
+        numbers.put("Second", 2);
+        numbers.put("Third", 3);
+        numbers.put("Fourth", 4);
+        System.out.println("TreeMap: " + numbers);
+    }
+}
