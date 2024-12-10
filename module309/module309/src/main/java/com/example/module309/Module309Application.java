f09595facd9bdee1024e5b070620c1857eaa54bb
Tony Shifflett <charles.l.shifflett@gmail.com>
2024-12-10 10:56:01 -0600

    initial commit springboot

---
+++ module309/module309/src/main/java/com/example/module309/Module309Application.java
@@ -0,0 +1,13 @@
+package com.example.module309;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Module309Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Module309Application.class, args);
+	}
+
+}
---
+++ module309/module309/src/test/java/com/example/module309/Module309ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.module309;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Module309ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
